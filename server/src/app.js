e6000e58ee61e10c2890fe1dda4e081be5259b46
David Frank <davidfrank96.df@gmail.com>
2019-07-02 08:38:25 -0700

    [chore] Setting up postgres database [finishes #167014723]

---
+++ server/src/app.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-console */
+import express from "express";
+import logger from "morgan";
+import { config } from "dotenv";
+import bodyParser from "body-parser";
+import "babel-polyfill";
+import apiRoutes from "./routes";
+//import ErrorHandler from "./middlewares/ErrorHandler";
+
+config();
+
+const app = express();
+const port = process.env.PORT || 9000;
+
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+
+app.use("/api", apiRoutes);
+app.use("*", (req, res) =>
+    res.status(404).json({
+        status: 404,
+        error: "Page Not Found"
+    })
+);
+
+//app.use(ErrorHandler.sendError);
+
+app.listen(port, () => {
+    console.log(`Listening from port ${port}`);
+});
+
+export default app;
