d711e3d950cb9bf4f623064a7c6a3554f550206f
David Frank <davidfrank96.df@gmail.com>
2019-07-09 04:51:10 -0700

    feat(Get/Trip-ApI): - Implemented a Get API to get all the trips made from the database -Both Users and Admin have access to the endpoint [Delivers #167131901]

--- server/src/controllers/TripsControllers.js
+++ server/src/controllers/TripsControllers.js
@@ -12,55 +12,81 @@ const {createTripQuery, findBusQuery, getAllTripsQuery, cancelTripQuery, filterQ
 
 
 class TripController {
-/**
-* @static
-* @description this method takes in the params from the req.body and creates a Trip
-* @param {object} req - Request object
-* @param {object} res - Response object
-* @memberof  TripController
-*/
+  /**
+   * @static
+   * @description this method takes in the params from the req.body and creates a Trip
+   * @param {object} req - Request object
+   * @param {object} res - Response object
+   * @memberof  TripController
+   */
   static createTrip(req, res) {
-    
-      const { bus_id, origin, destination, fare, } = req.body;
-        
-      const tripDetails = [bus_id, origin, destination, fare];
+    const { bus_id, origin, destination, fare } = req.body;
 
-      db.query(findBusQuery, [bus_id])
-        .then(response => {
-          const BusFound = response.rows[0];
+    const tripDetails = [bus_id, origin, destination, fare];
 
-              
-          if (!BusFound) {
-            res.status(400).json({
-              status: 400,
-              error: 'Bus with this ID  is not registered'
-            });
-            return;
-          }
+    db.query(findBusQuery, [bus_id])
+      .then(response => {
+        const BusFound = response.rows[0];
 
-          const BusActive = response.rows.filter(
-            eachBus => eachBus.status === "active"
-          );
+        if (!BusFound) {
+          res.status(400).json({
+            status: 400,
+            error: "Bus with this ID  is not registered"
+          });
+          return;
+        }
 
-          if (BusFound && BusActive.length) {
-            res.status(404).json({
-              status: 404,
-              error: 'Bus with this ID is already assigned'
-            });
-          } else {
-            db.query(createTripQuery, tripDetails)
-              .then(result2 => {
-                res.status(201).json({
-                  status: 201,
-                  data: "Trip  Successfully created"
-                });
-              })
-              .catch(err => console.log(err));
-          }
-        })
-        .catch(err => console.log(err));
-  
-    }  
-    
+        const BusActive = response.rows.filter(
+          eachBus => eachBus.status === "active"
+        );
+
+        if (BusFound && BusActive.length) {
+          res.status(404).json({
+            status: 404,
+            error: "Bus with this ID is already assigned"
+          });
+        } else {
+          db.query(createTripQuery, tripDetails)
+            .then(result2 => {
+              res.status(201).json({
+                status: 201,
+                data: "Trip  Successfully created"
+              });
+            })
+            .catch(err => console.log(err));
+        }
+      })
+      .catch(err => console.log(err));
+  }
+
+  static getTrips(req, res) {
+    db.query(getAllTripsQuery)
+      .then(response => {
+      
+        if (response.rows.length < 1) {
+          res.status(204).json({
+            status: 204,
+            data: "No trips available"
+          });
+        } else {
+          const trips = response.rows.map(item => ({
+            trip_id: item.id,
+            bus_id: item.bus_id,
+            origin: item.origin,
+            destination: item.destination,
+            trip_date: item.trip_date,
+            booking_status: item.booking_status,
+            fare: item.fare,
+            status: item.status
+          }));
+
+          res.status(200).json({
+            status: 200,
+            data: trips
+          });
+        }
+      })
+      .catch(err => console.log(err));
+  }
 }
 export default TripController;
