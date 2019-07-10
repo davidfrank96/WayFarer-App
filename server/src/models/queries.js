9662a84f62b1fffe1197a89b280995835ba42c18
David Frank <davidfrank96.df@gmail.com>
2019-07-10 14:19:22 -0700

    feat(Delete/book): This endpoint deletes a booking -This route deletes books in the database using the booking_id [Delivers #167132881]

--- server/src/models/queries.js
+++ server/src/models/queries.js
@@ -5,4 +5,9 @@ export default {
     getAllTripsQuery: 'SELECT * FROM trips ORDER BY id DESC',
     filterQuery: 'SELECT * FROM trips INNER JOIN buses ON trips.bus_id = buses.id WHERE trips.origin = $1',
     patchTripQuery: "UPDATE trips SET status = $1 WHERE id = $2 RETURNING *",
+    getTripsQuery: 'SELECT * FROM trips INNER JOIN buses ON trips.bus_id = buses.id WHERE buses.id = $1',
+    checkBookingQuery: 'SELECT * FROM bookings INNER JOIN users ON bookings.user_id = users.id WHERE user_id = $1 AND trip_id = $2',
+    updateTripQuery: 'UPDATE trips SET booking_status = $1 WHERE id = $2 RETURNING *',
+    bookingQuery: 'INSERT INTO bookings (user_id, trip_id, bus_id, trip_date, seat_number) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+
 };
