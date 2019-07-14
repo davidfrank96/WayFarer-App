c9608ad3ea9af7f75832af3b54561c64d95193a6
David Frank <davidfrank96.df@gmail.com>
2019-07-13 17:23:02 -0700

    data

--- server/src/test/utils/mockData.js
+++ server/src/test/utils/mockData.js
@@ -11,15 +11,6 @@ export default {
       createdAt: new Date(),
       modifiedAt: null
     },
-    validClientDetails: {
-      first_name: "Frank",
-      last_name: "Frank",
-      email: "frank@gmail.com",
-      password: hashedPassword("password", 10),
-      isAdmin: true,
-      createdAt: new Date(),
-      modifiedAt: null
-    },
     invalidUserDetails: {
       first_name: "Ashley",
       password: hashedPassword("password", 10),
@@ -44,8 +35,8 @@ export default {
   },
   Trip: {
     validTripDetails: {
-      bus_id: "1",
-      trip_id: 1,
+      bus_id: "4",
+      trip_id: 4,
       origin:"abuja",
       destination: "lagos",
       fare: 4000
