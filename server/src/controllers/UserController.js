13ee1ab6ce82ade890d2b2e97fccf10c2ed226c3
David Frank <davidfrank96.df@gmail.com>
2019-07-16 07:19:26 -0700

    isPassword

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -57,23 +57,23 @@ class UserController {
        const { rows } = await Users.find(email);
 
         console.log(rows[0]);
-        // if (!rows[0]) {
-        //     return res.status(401).json({
-        //         status: 401,
-        //         error: "Invalid credentials, inputed details does not match our records"
-        //     });
+        if (!rows[0]) {
+            return res.status(401).json({
+                status: 401,
+                error: "Invalid credentials, inputed details does not match our records"
+            });
 
-        // }
+        }
         const isPasswordValid = await UserController.verifyPassword(
             password,
             rows[0].password
         );
-        if (!isPasswordValid) {
-            return res.status(401).json({
-                status: 401,
-                error: "Invalid credentials, inputed details does not match our record"
-            });
-        }
+        // if (!isPasswordValid) {
+        //     return res.status(401).json({
+        //         status: 401,
+        //         error: "Invalid credentials, inputed details does not match our record"
+        //     });
+        // }
         const token = Authorization.generateToken(
             UserController.getTokenObj(rows[0])
         );
